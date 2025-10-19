baeb5a9be5e24b4b2f5a339af9b36ab58c4c7d0d
Farhan Sadik <farhansadik0760@gmail.com>
2025-10-19 21:50:23 +0600

    previous vite

--- vite.config.js
+++ vite.config.js
@@ -2,30 +2,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { sitemapPlugin } from "vite-plugin-sitemap";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react(),
-    sitemapPlugin({
-      hostname: "https://digitalhat.vercel.app", // 🔗 your live domain
-      outDir: "dist", // sitemap output directory
-      readable: true, // makes sitemap readable for debugging
-    }),
+    react()
   ],
-
-   build: {
-    chunkSizeWarningLimit: 1000, // default is 500 KB, now increased to 1000 KB
-    outDir: "dist",
-    minify: "esbuild",
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-
   server: {
     host: "0.0.0.0",
     port: 5173,
